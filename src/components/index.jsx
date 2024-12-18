9baa82dbe86d4ac08fe68259f458b8ec5dd1db84
daussysimon <daussy.simon@gmail.com>
2024-12-18 04:03:34 -0800

    performance v2

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -3,6 +3,5 @@ export * from "./layout";
 export * from "./header";
 export * from "./video";
 export * from "./gallery";
-export * from "./comments/comments-ui";
 export * from "./products/products";
 export * from "./contactForm";
